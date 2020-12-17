1610bdbc5cdd9fff77b28c8e3f0ee0968301af64
caugustoarruda <caugustogarruda@gmail.com>
2020-12-17 13:22:49 -0300

    Criando model UserLocation

--- join/core/models.py
+++ join/core/models.py
@@ -1,3 +1,14 @@
+import datetime
+
 from django.db import models
 
-# Create your models here.
+
+class UserLocation(models.Model):
+    nome = models.CharField(max_length=255, null=True, blank=True)
+
+    latitude = models.CharField(max_length=30, blank=True, null=True)
+    longitude = models.CharField(max_length=30, blank=True, null=True)
+    data_expiracao = models.DateTimeField(default=datetime.now())
+
+    def __str__(self):
+        return self.nome
