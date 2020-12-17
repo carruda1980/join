1079abdadc03f860cac002f2d3c0a2711285a0a1
caugustoarruda <caugustogarruda@gmail.com>
2020-12-17 13:32:09 -0300

    Model e primeira migração

--- join/core/models.py
+++ join/core/models.py
@@ -8,7 +8,7 @@ class UserLocation(models.Model):
 
     latitude = models.CharField(max_length=30, blank=True, null=True)
     longitude = models.CharField(max_length=30, blank=True, null=True)
-    data_expiracao = models.DateTimeField(default=datetime.now())
+    data_expiracao = models.DateTimeField(default=datetime.datetime.now())
 
     def __str__(self):
         return self.nome
--- join/settings.py
+++ join/settings.py
@@ -74,13 +74,22 @@ WSGI_APPLICATION = 'join.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'obrazul@2020',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
